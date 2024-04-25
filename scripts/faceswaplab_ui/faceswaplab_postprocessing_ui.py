4f1d0475d3ebf8caad3289317ecce9abac89b98a
Xkipper <yo.nibiru@gmail.com>
2024-04-24 23:57:37 -0400

    Update faceswaplab_postprocessing_ui.py

--- scripts/faceswaplab_ui/faceswaplab_postprocessing_ui.py
+++ scripts/faceswaplab_ui/faceswaplab_postprocessing_ui.py
@@ -5,6 +5,44 @@ from scripts.faceswaplab_postprocessing.postprocessing_options import Inpainting
 from scripts.faceswaplab_utils.sd_utils import get_sd_option
 from scripts.faceswaplab_ui.faceswaplab_inpainting_ui import face_inpainting_ui
 
+import traceback
+from scripts.faceswaplab_utils.typing import *
+#import requests
+from PIL import Image
+import scripts.faceswaplab_swapping.swapper as swapper
+from scripts.faceswaplab_utils.faceswaplab_logging import logger
+import os
+from modules import script_callbacks
+from pathlib import Path
+try:
+    from send2trash import send2trash
+    send2trash_installed = True
+except ImportError:
+    print("Delete Button: send2trash is not installed. recycle bin cannot be used.")
+    send2trash_installed = False
+
+
+def compareDos(img1: PILImage, img2: PILImage) -> str:
+    """
+    Compares the similarity between two faces extracted from images using cosine similarity.
+
+    Args:
+        img1: The first image containing a face.
+        img2: The second image containing a face.
+
+    Returns:
+        A str of a float value representing the similarity between the two faces (0 to 1).
+        Returns"You need 2 images to compare" if one or both of the images do not contain any faces.
+    """
+    try:
+        if img1 is not None and img2 is not None:
+            return str(round(float(swapper.compare_faces(img1, img2) * 100), 1)) + " %"
+    except Exception as e:
+        logger.error("Fail to compare", e)
+
+        traceback.print_exc()
+    return
+
 
 def postprocessing_ui() -> List[gr.components.Component]:
     with gr.Tab(f"Global Post-Processing"):
@@ -78,6 +116,49 @@ def postprocessing_ui() -> List[gr.components.Component]:
                 label="Enable/When",
             )
             global_inpainting = face_inpainting_ui("faceswaplab_gpp")
+    # -------------------------------------------
+    #   C O M P A R E   U I 
+    # -------------------------------------------
+    with gr.Tab("Compare"):        
+        with gr.Row():
+            attach_img1 = gr.Button("Attach generated to Face1", elem_id="faceswaplab_attach_btn")
+            delete_btn = gr.Button("\U0000274c Delete generated IMG", elem_id="faceswaplab_delete_btn")
+        
+        delete_imagen_txt = gr.Markdown(""" _Skipper 2024_ """)
+
+        gr.Markdown(""" ` """)
+
+        with gr.Row():
+            with gr.Blocks(show_footer=False) as blocks:
+                imge1 = gr.components.Image(
+				type="pil", label="Face 1", elem_id="faceswaplab_compare_face1",
+			    )
+
+            imge2 = gr.components.Image(
+				type="pil", label="Face 2", elem_id="faceswaplab_compare_face2",
+			)
+
+        gr.Markdown(""" _ """)
+        
+        """ compare_result_text = gr.Textbox(
+			interactive=False,
+			label="Similarity",
+			value="0",
+			elem_id="faceswaplab_compare_result",
+            container = True,
+		) """
+        compare_result_text = gr.Label(
+            label="Similarity",
+            elem_id="faceswaplab_compare_result",
+            container = True,
+            show_label = True,
+        )
+        attach_img1.click(attachGenerateIMG, outputs=[delete_imagen_txt, imge1])
+        imge1.change(compareDos, inputs=[imge1, imge2], outputs=[compare_result_text])
+        imge2.change(compareDos, inputs=[imge1, imge2], outputs=[compare_result_text])
+        
+
+        delete_btn.click(delete, outputs=[delete_imagen_txt])
 
     components = [
         face_restorer_name,
@@ -93,3 +174,40 @@ def postprocessing_ui() -> List[gr.components.Component]:
     for component in components:
         setattr(component, "do_not_save_to_config", True)
     return components
+
+
+# ============================
+#     DELETE IMG SECTION
+# ============================
+image_generate_path = ""
+
+
+def attachGenerateIMG():
+    global image_generate_path
+    if os.path.exists(image_generate_path):
+        image = Image.open(image_generate_path)
+        return [str(image_generate_path), image]
+
+
+def delete():
+    global image_generate_path
+    if os.path.exists(image_generate_path):
+        if send2trash_installed:
+            send2trash(image_generate_path)
+            return str("Send to TRASH »» " + image_generate_path )
+        else:
+            file = Path(image_generate_path)
+            file.unlink()
+            return str("DELETED! »» " + image_generate_path )
+    else:
+        return str("~ERROR~ Nothing to Delete...")
+    
+
+def on_image_saved(params : script_callbacks.ImageSaveParams):
+    global image_generate_path    
+    image_generate_path = os.path.realpath(params.filename)
+    
+
+script_callbacks.on_image_saved(on_image_saved)
+
+
